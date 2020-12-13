592751b4c6390a72f654a857f31af140543d18f7
Dane du Plessis <daneasc@gmail.com>
2020-12-13 21:48:46 +0200

    Hash maps find 3 sums easily

--- day1.dart
+++ day1.dart
@@ -1,17 +1,36 @@
-import 'dart:collection';
 import 'dart:io';
 
 main() async {
   File myFile = new File('day1.text');
   List<String> list = await myFile.readAsLines();
   print(list);
-  var numbers = <int>{};
+  Set<int> numbers = <int>{};
   numbers.addAll(list.map((e) => int.parse(e)));
   print(numbers);
+  // findPair(numbers, 2020);
+  findTriple(numbers);
+}
 
+void findTriple(Set<int> numbers) async {
   for (int i in numbers) {
-    if (numbers.contains(2020 - i)) {
-      print('and the number is: $i * ${2020 - i} = ${i * (2020 - i)}');
+    int target = 2020 - i;
+    // numbers.remove(i);
+    List success = await findPair(numbers, target);
+    if (success.length == 2) {
+      print(
+          'found it: $i, ${success[0]}, ${success[1]}, sum =${i + success[0] + success[1]}, product = ${i * success[0] * success[1]}');
+      return;
+    }
+    // numbers.add(i);
+  }
+}
+
+List<int> findPair(Set<int> numbers, int target) {
+  for (int i in numbers) {
+    if (numbers.contains(target - i)) {
+      print('and the number is: $i * ${target - i} = ${i * (target - i)}');
+      return [target - i, i];
     }
   }
+  return [];
 }
